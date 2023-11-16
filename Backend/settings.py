de0ed17d9805d4d890f842ab7678958367b91068
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-11-16 13:42:28 +0100

    all

--- Backend/settings.py
+++ Backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0ct!ew2$bfyf1lb596-r*w@-f6^@0860gi6y$u6uc30--@)x0q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
